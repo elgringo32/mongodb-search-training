b6db5a532607428b56a973547bc6e1885474a687
Brian K <bknopke@gmail.com>
2022-06-21 09:35:09 -0700

    Update server.js

--- server.js
+++ server.js
@@ -66,20 +66,3 @@ app.listen(process.env.PORT || PORT, () => {
     console.log(`Server is running.`)
 })
 
-//THIS IS THE INDEX TO APPLY TO MONGODB MOVIES COLLECTION
-// {
-//     "mappings": {
-//         "dynamic": false,
-//         "fields": {
-//             "title": [
-//                 {
-//                     "foldDiacritics": false,
-//                     "maxGrams": 7,
-//                     "minGrams": 3,
-//                     "tokenization": "edgeGram",
-//                     "type": "autocomplete"
-//                 }
-//             ]
-//         }
-//     }
-// }
